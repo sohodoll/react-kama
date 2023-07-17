cd33f32eb78aee3b7e71d959571c9a9c44f277dc
sohodoll <kovalevvtl@gmail.com>
2023-07-17 13:34:26 +0300

    feat: add ts utils

--- src/redux/sidebarReducer.ts
+++ src/redux/sidebarReducer.ts
@@ -2,12 +2,12 @@ export let friendsData = [
   { id: 1, name: 'Dimych' },
   { id: 2, name: 'Andrew' },
   { id: 3, name: 'Sveta' },
-];
+]
 
 const initialState = {
   friends: friendsData,
-};
+}
 
-export const sidebarReducer = (state = initialState, action) => {
-  return state;
-};
+export const sidebarReducer = (state = initialState) => {
+  return state
+}
